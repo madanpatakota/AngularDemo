af4a296508681360cf91e6dead89ee044d29764d
Madn <madan.patakota@gmail.com>
2024-06-13 10:43:07 +0530

    Basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,6 +26,66 @@ export class AppComponent {
      /*Property data bidning*/
       customerRole = "Admin";
       inputType = "radio";
+
+
+      /*Event data bidning*/
+      /*method or action or event*/
+      captureData($event:KeyboardEvent){
+        //console.log("Event triggered");
+        //console.log($event);
+        console.log($event.target);
+      }
+
+
+      
+      btnClick(){
+        console.log("button Fired!!!!");
+        window.alert("Message is " + this.ProjectName);  
+      }
+
+
+      ProjectDetails = "";
+      evtShowDetails(){
+        window.alert("Your message is " + this.ProjectDetails);
+      }
+
+
+
+
+      /*combine property and event databinding*/
+      cusPeterLocation = "London";
+      evtCusLocationKeyUp($event:KeyboardEvent){
+      // var inputEl =  $event.target;
+      // inputEl.
+      var inputEl2 =  $event.target as HTMLInputElement;
+      this.cusPeterLocation = inputEl2.value;
+      }
+
+
+
+
+      /*ngmodel and ngmodelChangecombine property and event databinding*/
+      cusJohnLocation = "New yark";
+      evtCusLocationModelChange($event:string){
+        this.cusJohnLocation = $event;
+      }
+
+
+
+      //[()]
+      guestName = "Robert";
+      guestLocation = "Colombo";
+
+
+
+
+
+
+
+
+
+
+
       
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { testComponent  } from './test/test.component';
 import { TestOneComponent } from './test-one/test-one.component';
+import { FormsModule } from '@angular/forms';
+
 @NgModule(
 {
                  // .html + .ts + .css + spec
   declarations: [AppComponent , testComponent, TestOneComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule,FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 }
