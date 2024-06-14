b1baf6f27fc1a1e7af7f80614682274145b61f87
Madn <madan.patakota@gmail.com>
2024-06-14 10:45:06 +0530

    ngstyle

--- src/app/comp-a/comp-a.component.ts
+++ src/app/comp-a/comp-a.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-comp-a',
   templateUrl: './comp-a.component.html',
   styleUrls: ['./comp-a.component.css']
 })
-export class CompAComponent implements OnInit {
+export class CompAComponent  {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  number = 1;
+  constructor() {
+     /* Ternary Operator : Simple way to handle the output 
+                           based on the condition. 
+                           its alternative of if else condition */
+
+    // if(this.number == 1){
+    //       console.log("One");
+    // }
+    // else{
+    //      console.log("not one");
+    // }
+
+
+    //this.number == 1 ?console.log("One") : console.log("Not One"); 
+
+
+
   }
+
+
+  divColor = 'yellow';
+
+
+  /*ngStyle*/
+  zoneSafeStatus = true;
+  zoneBorderStatus = true;
+  zonemaxWidth = false;
+
+
+
+
 
 }
