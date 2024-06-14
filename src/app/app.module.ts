683ae1202833ad9d8469780a79f0b20ba6fa46d6
Madn <madan.patakota@gmail.com>
2024-06-14 09:53:45 +0530

    second session

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { testComponent  } from './test/test.component';
 import { TestOneComponent } from './test-one/test-one.component';
 import { FormsModule } from '@angular/forms';
 import { CustomerSignupComponent } from './customer-signup/customer-signup.component';
+import { CompAComponent } from './comp-a/comp-a.component';
 
 @NgModule(
 {
                  // .html + .ts + .css + spec
-  declarations: [AppComponent , testComponent, TestOneComponent, CustomerSignupComponent],
+  declarations: [AppComponent , testComponent, TestOneComponent, CustomerSignupComponent, CompAComponent],
   imports: [BrowserModule,FormsModule],
   providers: [],
   bootstrap: [AppComponent]
